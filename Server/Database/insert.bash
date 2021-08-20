c791ac49a3cf3f39a78f581ddaf8b46e45f1d6fc
yeranosyanvahan <yeranosyanvahan@gmail.com>
2021-08-20 08:36:21 +0400

    Adding Graphql

--- Server/Database/insert.bash
+++ Server/Database/insert.bash
@@ -1,10 +1,12 @@
 echo Inserting Schema
-curl -H "Content-Type: application/rdf" -4 --retry 3 --retry-connrefused --retry-delay 10 --connect-timeout 5 localhost:8080/alter --request POST --data-binary @Preload/Schema
+curl -H "Content-Type: application/rdf" -4 --retry 3 --retry-connrefused --retry-delay 10 --connect-timeout 5 localhost:8080/alter --request POST --data-binary @DQL/Schema
 echo Adding Files
-echo "{set{" > ./Preload/Mutation
-cat Preload/Store >>  ./Preload/Mutation
-cat Preload/Properties >>  ./Preload/Mutation
-cat Preload/Product >>  ./Preload/Mutation
-cat Preload/Categories >>  ./Preload/Mutation
-echo "}}" >> ./Preload/Mutation
-curl -H "Content-Type: application/rdf" -X POST localhost:8080/mutate?commitNow=true --data-binary @Preload/Mutation
+echo "{set{" > ./DQL/Mutation
+cat DQL/Store >>  ./DQL/Mutation
+cat DQL/Properties >>  ./DQL/Mutation
+cat DQL/Product >>  ./DQL/Mutation
+cat DQL/Categories >>  ./DQL/Mutation
+echo "}}" >> ./DQL/Mutation
+curl -H "Content-Type: application/rdf" -X POST localhost:8080/mutate?commitNow=true --data-binary @DQL/Mutation
+curl -H "Content-Type: application/graphql" -X POST localhost:8080/mutate?commitNow=true --data-binary @Graphql/Gschema
+curl -H "Content-Type: application/graphql" -X POST localhost:8080/mutate?commitNow=true --data-binary @Graphql/AddCategory
