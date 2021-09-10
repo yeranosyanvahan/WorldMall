cf8cc422b5168c20f9fdc0470f74e681a1e8ec23
yeranosyanvahan <yeranosyanvahan@gmail.com>
2021-09-10 23:32:27 +0400

    Sleep more for database preparation

--- Server/Database/insert.bash
+++ Server/Database/insert.bash
@@ -9,7 +9,7 @@ cat DQL/Categories >>  ./DQL/Mutation
 echo "}}" >> ./DQL/Mutation
 curl -H "Content-Type: application/rdf" -X POST localhost:8080/mutate?commitNow=true --data-binary @DQL/Mutation
 
-sleep 2s
+sleep 10s
 echo "Start Adding GraphQL"
 curl -X POST localhost:8080/admin/schema --data-binary @Graphql/Gschema
 curl -H "Content-Type: application/graphql" -X POST localhost:8080/graphql --data-binary @Graphql/AddCategory
