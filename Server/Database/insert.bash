cfb7c901240ec397cf91b760e7d099669311df94
yeranosyanvahan <yeranosyanvahan@gmail.com>
2021-09-10 23:53:13 +0400

    Updating insert bash

--- Server/Database/insert.bash
+++ Server/Database/insert.bash
@@ -1,5 +1,5 @@
 echo Inserting Schema
-curl -H "Content-Type: application/rdf" -4 --retry 3 --retry-connrefused --retry-delay 10 --connect-timeout 5 localhost:8080/alter --request POST --data-binary @DQL/Schema
+#curl -H "Content-Type: application/rdf" -4 --retry 3 --retry-connrefused --retry-delay 10 --connect-timeout 5 localhost:8080/alter --request POST --data-binary @DQL/Schema
 echo Adding Files
 echo "{set{" > ./DQL/Mutation
 cat DQL/Store >>  ./DQL/Mutation
@@ -7,7 +7,7 @@ cat DQL/Properties >>  ./DQL/Mutation
 cat DQL/Product >>  ./DQL/Mutation
 cat DQL/Categories >>  ./DQL/Mutation
 echo "}}" >> ./DQL/Mutation
-curl -H "Content-Type: application/rdf" -X POST localhost:8080/mutate?commitNow=true --data-binary @DQL/Mutation
+#curl -H "Content-Type: application/rdf" -X POST localhost:8080/mutate?commitNow=true --data-binary @DQL/Mutation
 
 sleep 10s
 echo "Start Adding GraphQL"
